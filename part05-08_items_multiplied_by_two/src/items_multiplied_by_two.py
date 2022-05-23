602b3e8e18c49d094ca066182c4c6b6b3c771f0c
flankticus <ibad.scet@gmail.com>
2022-05-23 09:46:58 +0530

    part-05 exercises

---
+++ part05-08_items_multiplied_by_two/src/items_multiplied_by_two.py
@@ -0,0 +1,13 @@
+# Write your solution here
+def double_items(alist: list):
+    new_list = alist[:]
+    for i in range(len(alist)):
+        new_list[i] *= 2
+    return new_list
+
+
+if __name__ == "__main__":
+    numbers = [2, 4, 5, 3, 11, -4]
+    numbers_doubled = double_items(numbers)
+    print("original:", numbers)
+    print("doubled:", numbers_doubled)
